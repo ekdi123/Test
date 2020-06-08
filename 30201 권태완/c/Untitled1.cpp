365327d06defa710ff25c833df169e961a714af2
ekdi123 <66227841+ekdi123@users.noreply.github.com>
2020-06-08 09:27:21 +0900

    우와
    신기

---
+++ 30201 권태완/c/Untitled1.cpp
@@ -0,0 +1,11 @@
+#include <Stdio.h>
+int main()
+{
+	int a=0;
+	for(int i=1;i<=10;i++)
+	{
+		printf("%d\n",i);
+		 a= a+i;
+	}
+	printf("%d\n",a);
+}
---
+++ 30201 권태완/자바/oh3.java
@@ -0,0 +1,15 @@
+package oh2;
+
+public class oh3 {
+
+	public static void main(String[] args) {
+		int a=0;
+		for (int i = 1; i<=10; i++){
+			System.out.println("ÀÏ°ª"+i);
+			a += i;
+			
+		}
+		System.out.println("ÇÕ"+a);
+	}
+
+}
